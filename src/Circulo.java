b18a8ee30d767e40d38433316fc4755d4d2584bb
VictoriaRipa <112644052+VictoriaRipa@users.noreply.github.com>
2022-11-24 18:41:07 -0300

    Update Circulo.java
    agregue calculo de radio

--- src/Circulo.java
+++ src/Circulo.java
@@ -1,14 +1,17 @@
 public class Circulo extends Forma{
 
-    public double radio;
+    public double radio=1.0;
 
-    //metodos
+    public Circulo (){
+
+    }
+    public Circulo (double radio) {this.radio=radio;}
 
     public Circulo (double radio, String color, boolean relleno){
         super(color, relleno);
         this.radio = radio;
     }
-    
+
 
     //getters y setters
     public double getRadio() {
@@ -17,5 +20,15 @@ public class Circulo extends Forma{
 
     public void setRadio(double radio) {
         this.radio = radio;
+    }
+    
+    @Override
+    public double getArea() {
+        return getRadio()*2*3.14;
+    }
+
+    @Override
+    public double getPerimetro() {
+        return getRadio()*getRadio()*3.14;
     }
 }
